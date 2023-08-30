47c014ec091b055f0623bb0902340ee5474de466
Aryan209e <aryansiyag22@gmail.com>
2023-08-30 15:42:14 +0530

    basic add of js

---
+++ basic.js
@@ -0,0 +1,12 @@
+
+
+1 - The Basics
+01 - Welcome.
+02 - Browser, Editor and Terminal Setup.
+03 - Running and Loading JavaScript.
+04 - Variables and Statements.
+05 - Code Quality Tooling with Prettier and ESLint.
+06 - Types - Introduction.
+07 - Types - Strings.
+08 - Types - Numbers.
+
